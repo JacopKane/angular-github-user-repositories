70e530f4bac777f65f79bd5810a54ed04e743dc4
Furkan Tunali <furkan@nod.st>
2016-07-22 17:08:03 +0300

    Update 2016-07-22T14:07:58.137Z

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-const
-
-  app = angular.module('app', [
-    'template-cache',
-    'github-user-repositories'
-  ])
-    .config(['$logProvider', function ($logProvider) {
-      $logProvider.debugEnabled(false);
-    }]);
